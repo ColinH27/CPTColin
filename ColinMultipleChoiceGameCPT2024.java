084ed0b796ec1d2fb0b54cf8deee1dbd73d903cc
ColinH27 <Colin.hu26@ycdsbk12.ca>
2025-01-09 09:29:40 -0500

    Added Score txt so that every time someone plays there score will be put into the txt file

--- ColinMultipleChoiceGameCPT2024.java
+++ ColinMultipleChoiceGameCPT2024.java
@@ -25,12 +25,16 @@ public class ColinMultipleChoiceGameCPT2024 {
 
 		while (true) {
 	// 	ASK PLAYER FOR THEIR NAME
-		con.println("Enter your name");
+   		con.println("Enter your name");
 		strName = con.readLine();
+		con.println("Welcome " + strName + "!");
+		con.sleep(1000);
 		con.clear();
-	
-        con.println("What would you like to do?");
+  
+    // MAIN MENU SCREEN
+		con.println("What would you like to do?");
         strMenuChoice = con.readLine();
+        con.sleep(1000);
         con.clear();
 
         // STA TITAN "CHEAT"
@@ -38,25 +42,12 @@ public class ColinMultipleChoiceGameCPT2024 {
        con.println("It's your lucky day!!!!! :)");
        con.println("Congrats!");
        con.println("To thank you 5 points have been added to total score!");
-       intTotalScore = 5 + intTotalScore;
+       // ADD Something
         }
 
         // VIEW SCORE SCREEN
 	if (strMenuChoice.equalsIgnoreCase("s")) {
-        con.println("Type 'mcu' to view marvel test score!");
-        con.println("Type 'sw' to view star wars test score!");
-        con.println("Type 'c' to view guess the country test score!");
-         con.println("Which score would you like to view?");
-            String strScore = con.readLine();
-
-            if (strScore.equalsIgnoreCase("mcu")) {
-                con.println("Score: " + intMCUScore);
-            } else if (strScore.equalsIgnoreCase("sw")) {
-                con.println("Score: " + intStarWarsScore);
-            } else if (strScore.equalsIgnoreCase("dc")) {
-                con.println("Score: " + intDCScore);
-       
-            }
+        // ADD LEADERBOARD
         }
 
         // HELP SCREEN (TUTORIAL)
@@ -102,7 +93,7 @@ public class ColinMultipleChoiceGameCPT2024 {
                 strMCU[intRow][3] = MCU.readLine(); // Option C
                 strMCU[intRow][4] = MCU.readLine(); // Option D
                 strMCU[intRow][5] = MCU.readLine(); // Correct Answer
-
+				// PRINTS QUESTION AND OPTIONS
                 con.println("Question " + (intRow + 1) + ":");
                 con.println(strMCU[intRow][0]);
                 con.println(strMCU[intRow][1]);
@@ -110,27 +101,39 @@ public class ColinMultipleChoiceGameCPT2024 {
                 con.println(strMCU[intRow][3]);
                 con.println(strMCU[intRow][4]);
 				
-				
+				// PLAYER ENTERS ANSWER
                 con.print("Enter your answer: ");
                 String strChoice = con.readLine();
                 String correctAnswer = strMCU[intRow][5].substring(strMCU[intRow][5].length() - 1);
+     
+     // CHECKS IF ANSWER IS CORRECT OR NOT
       if (strChoice.equalsIgnoreCase(correctAnswer)) {
           con.println("Correct!");
-         intMCUScore = intMCUScore + 1;
+     // SCORING
+           intMCUScore = intMCUScore + 1;
+
          } else {
  con.println("Incorrect. The correct answer is " + correctAnswer + ".");
                 }
          // Clears and moves onto next question       
          con.sleep(3000);
          con.clear();
-
+	
+	     TextOutputFile Scorefile = new TextOutputFile("Score.txt");
+         Scorefile.println(strName);
+         Scorefile.println("MCU TEST");
+         Scorefile.println(intMCUScore + "/10");
                 
             }
+	
+		// Final Score Screen & Thank You Screen
+		con.println("Your Final Score: " + intMCUScore + "!");
 
        // Thank you for playing Screen
        con.println("What would you like to do now?");
        con.println("Type 'Quit' to quit or 'MM' to go back to the Menu");
        strEnd = con.readLine();
+       con.clear();
         }
 
         // STAR WARS TEST
@@ -170,8 +173,15 @@ public class ColinMultipleChoiceGameCPT2024 {
                 String correctAnswer = strStarWars[intRow][5].substring(strStarWars[intRow][5].length() - 1);
 
 	if (strChoice.equalsIgnoreCase(correctAnswer)) {
-		con.println("Correct!");
-			intStarWarsScore = intStarWarsScore + 1;
+		con.println("Correct!");	
+		// SCORING
+		intStarWarsScore = intStarWarsScore + 1;
+         TextOutputFile Scorefile = new TextOutputFile("Score.txt");
+         Scorefile.println(strName);
+         Scorefile.println("STAR WARS TEST");
+         Scorefile.println(intStarWarsScore + "/10");
+                 
+			
 	} else {
 	con.println("Incorrect. The correct answer is " + correctAnswer + ".");
                 }
@@ -180,6 +190,15 @@ public class ColinMultipleChoiceGameCPT2024 {
          con.sleep(3000);
          con.clear();
             }
+            
+          // Final Score Screen & Thank You Screen
+		con.println("Your Final Score" + intStarWarsScore + "!");
+		
+	// Thank you for playing Screen
+       con.println("What would you like to do now?");
+       con.println("Type 'Quit' to quit or 'MM' to go back to the Menu");
+       strEnd = con.readLine();   
+       con.clear();
             
         }
 
@@ -207,21 +226,28 @@ public class ColinMultipleChoiceGameCPT2024 {
                 strDC[intRow][3] = DC.readLine();
                 strDC[intRow][4] = DC.readLine();
                 strDC[intRow][5] = DC.readLine();
-
+	
+				// PRINTS QUESTION AND OPTIONS
                 con.println("Question " + (intRow + 1) + ":");
                 con.println(strDC[intRow][0]);
                 con.println(strDC[intRow][1]);
                 con.println(strDC[intRow][2]);
                 con.println(strDC[intRow][3]);
                 con.println(strDC[intRow][4]);
-
+				
+				// PLAYER ENTERS THEIR ANSWER
                 con.print("Enter your answer: ");
                 String strChoice = con.readLine();
                 String correctAnswer = strDC[intRow][5].substring(strDC[intRow][5].length() - 1);
 
       if (strChoice.equalsIgnoreCase(correctAnswer)) {
             con.println("Correct!");
-            intDCScore = intDCScore + 1;
+         // SCORING
+         intDCScore = intDCScore + 1;		
+         TextOutputFile Scorefile = new TextOutputFile("Score.txt");
+         Scorefile.println(strName);
+         Scorefile.println("DC TEST");
+         Scorefile.println(intDCScore + "/10");
       } else {
  con.println("Incorrect. The correct answer is " + correctAnswer + ".");
 					}
@@ -231,8 +257,21 @@ public class ColinMultipleChoiceGameCPT2024 {
 				}
 				
 			}
+		// Final Score Screen & Thank You Screen
+		con.println("Your Final Score: " + intDCScore + "!");
+			
+		// Thank you for playing Screen
+       con.println("What would you like to do now?");
+       con.println("Type 'Quit' to quit or 'MM' to go back to the Menu");
+       strEnd = con.readLine();
+       con.clear();
+			
 		}
 	
+
+
+
+		
 	
 	
 	
